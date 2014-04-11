022984889711f5ab3b08826d96f391343ce2e9a6
Chad Bailey <chadbailey@gmail.com>
2014-04-11 11:19:03 -0500

    doh

--- config/initializers/rack-latency.rb
+++ config/initializers/rack-latency.rb
@@ -1,7 +1,7 @@
 Rack::Latency.configure do |measure|
   measure.head "http://74.125.227.167", name: "google" # google.com ipv4
   measure.head "http://98.138.253.109", name: "yahoo" # yahoo.com ipv4
-  measure.head "http://help.heroku.com"
-  measure.head "http://health-reprort.herokuapp.com"
+  measure.head "http://help.heroku.com", name: "help"
+  measure.head "http://health-report.herokuapp.com", name: "health-report"
   measure.wait 4
 end
